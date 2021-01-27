1a02792141869e951c7befa8c83b3ac099d746a9
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-01-27 18:27:04 -0300

    feat: add the quiz database

---
+++ db.ts
@@ -0,0 +1,22 @@
+export default {
+  bg: 'https://placehold.it/1368x768',
+  title: 'Título do Quiz',
+  description: 'Descrição do quiz aqui',
+  questions: [
+    {
+      image: 'https://placehold.it/400x400',
+      title: 'Questão número 1',
+      description: 'Descrição da questão #1',
+      answer: 1,
+      alternatives: [
+        'Alternativa A',
+        'Alternativa B',
+        'Alternativa C',
+        'Alternative D'
+      ]
+    }
+  ],
+  external: [
+    'https://github.com/Matheus-Campos'
+  ],
+}
