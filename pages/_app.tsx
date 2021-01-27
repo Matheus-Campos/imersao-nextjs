42822de71ab4913253ed3ddcdb3c48d91ff13ad3
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-01-27 17:57:00 -0300

    feat: add styled-components (with typescript)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,32 @@
 import { FC } from 'react'
 import { AppProps } from 'next/app'
+
+import { createGlobalStyle, ThemeProvider, DefaultTheme } from 'styled-components'
 import '../styles/globals.css'
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#0070f3'
+  }
+}
+
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
